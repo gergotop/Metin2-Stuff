321106d8c507dbb2b93351ab1f81b21263b5f508
Gergo Kalman <kgyorgygergo@gmail.com>
2021-06-02 16:04:23 +0200

    Added client optimalization

---
+++ Client-Optimization/howto.cpp
@@ -0,0 +1,43 @@
+Server
+Open the file game/main.cpp
+#################################################
+
+Search this function:
+#################################################
+
+int main(int argc, char **argv)
+{
+
+After this add the following code:
+#################################################
+
+return 0;
+
+Now open the file db/Main.cpp.
+#################################################
+
+Search this function:
+#################################################
+
+int main()
+{
+
+After this add the following code:
+#################################################
+
+return 0;
+
+Client
+Open the file UserInterface/UserInterface.cpp
+#################################################
+
+Search this function:
+#################################################
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+
+After this add the following code:
+#################################################
+
+return 0;
