6f3dffe1eba5da47e62ebdf01fb1208bfb161f53
Gergo Kalman <kgyorgygergo@gmail.com>
2021-06-09 09:57:26 +0200

    default sources

---
+++ GCC9-SRC/Server-Source/common/service.h
@@ -0,0 +1,9 @@
+#ifndef __INC_SERVICE_H__
+#define __INC_SERVICE_H__
+
+#define _IMPROVED_PACKET_ENCRYPTION_ // 패킷 암호화 개선
+//#define __AUCTION__
+#define __PET_SYSTEM__
+#define ENABLE_PORT_SECURITY
+#define __UDP_BLOCK__
+#endif
---
+++ GCC9-SRC/Server-Source/common/utils.h
@@ -0,0 +1,128 @@
+#pragma once
+/*----- atoi function -----*/
+inline bool str_to_number (bool& out, const char *in)
+{
+	if (0==in || 0==in[0])	return false;
+
+	out = (strtol(in, NULL, 10) != 0);
+	return true;
+}
+
+inline bool str_to_number (char& out, const char *in)
+{
+	if (0==in || 0==in[0])	return false;
+
+	out = (char) strtol(in, NULL, 10);
+	return true;
+}
+
+inline bool str_to_number (unsigned char& out, const char *in)
+{
+	if (0==in || 0==in[0])	return false;
+
+	out = (unsigned char) strtoul(in, NULL, 10);
+	return true;
+}
+
+inline bool str_to_number (short& out, const char *in)
+{
+	if (0==in || 0==in[0])	return false;
+
+	out = (short) strtol(in, NULL, 10);
+	return true;
+}
+
+inline bool str_to_number (unsigned short& out, const char *in)
+{
+	if (0==in || 0==in[0])	return false;
+
+	out = (unsigned short) strtoul(in, NULL, 10);
+	return true;
+}
+
+inline bool str_to_number (int& out, const char *in)
+{
+	if (0==in || 0==in[0])	return false;
+
+	out = (int) strtol(in, NULL, 10);
+	return true;
+}
+
+inline bool str_to_number (unsigned int& out, const char *in)
+{
+	if (0==in || 0==in[0])	return false;
+
+	out = (unsigned int) strtoul(in, NULL, 10);
+	return true;
+}
+
+inline bool str_to_number (long& out, const char *in)
+{
+	if (0==in || 0==in[0])	return false;
+
+	out = (long) strtol(in, NULL, 10);
+	return true;
+}
+
+inline bool str_to_number (unsigned long& out, const char *in)
+{
+	if (0==in || 0==in[0])	return false;
+
+	out = (unsigned long) strtoul(in, NULL, 10);
+	return true;
+}
+
+inline bool str_to_number (long long& out, const char *in)
+{
+	if (0==in || 0==in[0])	return false;
+
+	out = (long long) strtoull(in, NULL, 10);
+	return true;
+}
+
+inline bool str_to_number (float& out, const char *in)
+{
+	if (0==in || 0==in[0])	return false;
+
+	out = (float) strtof(in, NULL);
+	return true;
+}
+
+inline bool str_to_number (double& out, const char *in)
+{
+	if (0==in || 0==in[0])	return false;
+
+	out = (double) strtod(in, NULL);
+	return true;
+}
+
+#ifdef __FreeBSD__
+inline bool str_to_number (long double& out, const char *in)
+{
+	if (0==in || 0==in[0])	return false;
+
+	out = (long double) strtold(in, NULL);
+	return true;
+}
+#endif
+
+template<typename X, typename Y>
+constexpr const auto MIN(X a, Y b)
+{
+	return (a < b ? a : b);
+}
+
+template<typename X, typename Y>
+constexpr const auto MAX(X a, Y b)
+{
+	return (a > b ? a : b);
+}
+
+template<typename X, typename Y, typename Z>
+constexpr const auto MINMAX(X min, Y value, Z max)
+{
+	return (value < min ? min : (value > max ? max : value));
+}
+
+
+/*----- atoi function -----*/
