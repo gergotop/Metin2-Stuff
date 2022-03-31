172e40d3edcc25269e09e3624694f52001b4dc02
Gergo Kalman <kgyorgygergo@gmail.com>
2022-03-31 10:46:57 +0200

    Installer Files

--- ServerInstaller/database.sh
+++ ServerInstaller/database.sh
@@ -1,5 +1,5 @@
-mysql -uroot < database/db_create.sql
-mysql -uroot < database/mt2_user_c.sql
+mysql -uroot < database/base/db_create.sql
+mysql -uroot < database/base/mt2_user_c.sql
 mysql -uroot account < database/account.sql
 mysql -uroot common < database/common.sql
 mysql -uroot log < database/log.sql
