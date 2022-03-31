462177a8d5e43ced38e8b02b32d7bbcfe4071fd9
Gergo Kalman <kgyorgygergo@gmail.com>
2022-03-31 10:49:56 +0200

    Installer Files

--- ServerInstaller/database/base/mt2_user_c.sql
+++ ServerInstaller/database/base/mt2_user_c.sql
@@ -1,12 +1,12 @@
 CREATE USER 'metin2'@'localhost' IDENTIFIED BY 'password';
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON `srv1_player`.* TO 'metin2'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `player`.* TO 'metin2'@'localhost';
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON `srv1_log`.* TO 'metin2'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `log`.* TO 'metin2'@'localhost';
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON `srv1_hotbackup`.* TO 'metin2'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `hotbackup`.* TO 'metin2'@'localhost';
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON `srv1_common`.* TO 'metin2'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `common`.* TO 'metin2'@'localhost';
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON `srv1_account`.* TO 'metin2'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `account`.* TO 'metin2'@'localhost';
 
