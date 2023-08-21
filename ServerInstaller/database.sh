d9f4fc308f72e46361eae798e494f337aeab280a
Kálmán Gergő <kgyorgygergo@gmail.com>
2023-08-21 13:29:42 +0200

    Update database.sh

--- ServerInstaller/database.sh
+++ ServerInstaller/database.sh
@@ -1,6 +1,8 @@
-mysql -uroot < database/base/db_create.sql
-mysql -uroot < database/base/mt2_user_c.sql
-mysql -uroot account < database/account.sql
-mysql -uroot common < database/common.sql
-mysql -uroot log < database/log.sql
-mysql -uroot player < database/player.sql
+mysql -uroot -ppassword < database/base/db_create.sql
+mysql -uroot -ppassword < database/base/mt2_user_c.sql
+
+## Create clean metin2 db struct
+mysql -uroot -ppassword account < database/account.sql
+mysql -uroot -ppassword common < database/common.sql
+mysql -uroot -ppassword log < database/log.sql
+mysql -uroot -ppassword player < database/player.sql
